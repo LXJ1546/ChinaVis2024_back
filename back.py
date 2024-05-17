dc9a33f8b0dd158bb8213de31bce575f7948c500
hsiif <1851507245@qq.com>
2024-05-17 20:30:16 +0800

    basic_info更新

--- back.py
+++ back.py
@@ -19,12 +19,20 @@ def basicInfo():
     file_temp = './data/classes/basic_info_'
     file = file_temp+'1.csv'
     data = pd.read_csv(file).sort_values(by='all_knowledge', ascending=False)
-    result = []
+    result_each = []  # 每个学生分别的信息
+    result_all = []  # 总的信息，比如每个专业的人数
     for index, row in data.iterrows():
-        result.append([row['student_ID'], row['major'], row['age'],
-                       row['sex'], row['all_knowledge'], 'class1'])
-    # print(result)
+        result_each.append([row['student_ID'], row['major'], row['age'],
+                            row['sex'], row['all_knowledge'], 'class1'])
 
+    result_all.append([data['major'].value_counts().index.tolist(),
+                       data['major'].value_counts().values.tolist()])
+    result_all.append([data['age'].value_counts().sort_index().index.tolist(),
+                       data['age'].value_counts().sort_index().values.tolist()])
+    result_all.append([data['sex'].value_counts().index.tolist(),
+                       data['sex'].value_counts().values.tolist()])
+
+    result = [result_all, result_each]
     return (result)
 
 
