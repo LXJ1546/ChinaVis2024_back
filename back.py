d164507b79f94a34551098d66c9284be7354d2e8
hsiif <1851507245@qq.com>
2024-05-17 19:59:17 +0800

    basic_info

--- back.py
+++ back.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+import pandas as pd
+
 
 app = Flask(__name__)
 # 允许跨域传输数据
@@ -9,6 +11,21 @@ CORS(app)
 @app.route('/back')
 def hello():
     return 'Hello, World!'
+
+
+@app.route('/basicInfo')
+def basicInfo():
+    # 后续需要根据班级获取不同班的数据
+    file_temp = './data/classes/basic_info_'
+    file = file_temp+'1.csv'
+    data = pd.read_csv(file).sort_values(by='all_knowledge', ascending=False)
+    result = []
+    for index, row in data.iterrows():
+        result.append([row['student_ID'], row['major'], row['age'],
+                       row['sex'], row['all_knowledge'], 'class1'])
+    # print(result)
+
+    return (result)
 
 
 if __name__ == '__main__':
